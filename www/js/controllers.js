281046968bbbc4b3f4f991e03b8c156963cdffe5
Benjamin Skipper <BSkipper@commandalkon.com>
2015-12-22 11:43:16 -0600

    truck code now properly saves to local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,63 +1,49 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
 
-.controller('truckCodeCtrl', function($scope, TicketService, $state) {
+  .controller('truckCodeCtrl', function ($scope, TicketService, $state) {
 
-  $scope.setTruckCode = function(truckCode)
-  {
-
-    console.log('TruckCode Set to ' + truckCode);
-    TicketService.setTruckCode(truckCode);
-    console.log('TruckCode saved to TicketService as ' + TicketService.truckCode);
-    $state.go('ticket.waitView')
-  };
-})
-
-  .controller('waitCtrl', function($scope, TicketService, $state) {
-
-    $scope.truckCode = TicketService.truckCode;
-    $scope.setTruckCode = function(truckCode)
-    {
+    $scope.setTruckCode = function (truckCode) {
 
       console.log('TruckCode Set to ' + truckCode);
       TicketService.setTruckCode(truckCode);
@@ -66,5 +52,12 @@ angular.module('starter.controllers', [])
     };
   })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+  .controller('waitCtrl', function ($scope, TicketService, $state, $timeout) {
+
+    $timeout(function () {
+      $scope.truckCode = TicketService.getTruckCode();
+    })
+  })
+
+  .controller('PlaylistCtrl', function ($scope, $stateParams) {
+  });
--- www/js/services/ticketService.js
+++ www/js/services/ticketService.js
@@ -2,10 +2,24 @@ angular.module('ticket.services', [])
 
 .service('TicketService', function(){
 
-  this.truckCode = '';
+  var self = this;
 
-  this.setTruckCode = function(truckCodeIn){
-    this.truckCode = truckCodeIn;
-  }
+  self.truckCode;
 
+
+
+  var setup = function(){
+    self.truckCode = self.truckCode || localStorage.truckCode;
+  };
+
+  self.setTruckCode = function(truckCodeIn){
+    self.truckCode = truckCodeIn;
+    localStorage.truckCode = truckCodeIn;
+  };
+
+  self.getTruckCode = function(){
+    return self.truckCode;
+  };
+
+  setup();
 });
