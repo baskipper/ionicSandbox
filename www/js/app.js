a369ee302cb27d6fed5547e2edd84061055dc4af
BSkipper <bskipper@commandalkon.com>
2016-01-12 09:14:09 -0600

    re-added ionic-timepicker for demo purposes

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ticket.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'ionic-timepicker', 'ticket.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
